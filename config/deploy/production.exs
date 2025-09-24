c565f05a630fa19547f87e85ff27aeba43e6ef54
Andrew Dixon <dixon@largearcade.com>
2025-09-24 09:34:29 -0700

    Configure Deploy

--- config/deploy/production.exs
+++ config/deploy/production.exs
@@ -9,8 +9,8 @@ use Bootleg.DSL
 #  - `identity`: local path to an identity file that will be used for SSH authentication instead of a password
 #  - `workspace`: remote file system path to be used for building and deploying this Elixir project
 
-role :app, ["staging.haikuter.com"],
-  workspace: "/var/www/bootleg_test",
+role :app, ["localhost"],
+  workspace: "/var/www/bootleg",
   user: "builder",
   identity: "~/.ssh/id_ed25519",
   silently_accept_hosts: true

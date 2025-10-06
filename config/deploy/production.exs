afc0715987c2a2ed655d19a8ef871af3d6a91a39
Andrew Dixon <dixon@largearcade.com>
2025-10-06 10:17:44 -0700

    Working on Self Signed Cert
    - openssl is documented poorly
    - working without config file (Interactively)
    - complexity creates an opportunity to be the one to solve the problem

--- config/deploy/production.exs
+++ config/deploy/production.exs
@@ -21,16 +21,18 @@ role :app, ["bootleg-test.com"], workspace: "/var/www/bootleg",
 task :init_systemd do
   UI.info(IO.ANSI.magenta() <> "Generating SystemD Unit File..." <> IO.ANSI.reset())
 
-  description = Mix.Project.config()[:description]
-  app_name = Mix.Project.config()[:app]
-  mix_env = config({:mix_env, "prod"})
   build_role = Config.get_role(:app).hosts |> Enum.at(0)
-  host_name = build_role.host.name
-  app_port = build_role.options[:app_port]
-  user = Config.get_role(:app).user
-  workspace = Config.get_role(:app).options[:workspace]
-  {output, _} = System.cmd("mix", ["phx.gen.secret"])
+  host_name  = build_role.host.name
+  app_port   = build_role.options[:app_port]
+
+  {output, _}     = System.cmd("mix", ["phx.gen.secret"])
   secret_key_base = String.split(output, "\n", trim: true) |> Enum.at(-1)
+
+  description  = Mix.Project.config()[:description]
+  app_name     = Mix.Project.config()[:app]
+  mix_env      = config({:mix_env, "prod"})
+  user         = Config.get_role(:app).user
+  workspace    = Config.get_role(:app).options[:workspace]
   database_url = "ecto://postgres:postgres@#{host_name}/#{app_name}_#{mix_env}"
 
   unit_file_template = "config/systemd/application.service.eex"
@@ -122,16 +124,56 @@ task :self_signed_cert do
   cert_dir = "#{File.cwd!()}/releases/self_signed_cert"
   File.mkdir_p!(cert_dir)
 
+  app_name = Mix.Project.config()[:app]
   build_role = Config.get_role(:app).hosts |> Enum.at(0)
   host_name = build_role.host.name
+  ca_config_template = "config/self_signed_cert/ca.conf.eex"
+  signing_request_config_template = "config/self_signed_cert/signing_request.conf.eex"
+  extensions_config_template = "config/self_signed_cert/extensions.conf.eex"
+  shell_args = [cd: cert_dir, into: IO.stream()]
 
   ######################
   # Become a Certificate Authority
   ######################
-  # Generate private key
-  openssl_genrsa = "openssl genrsa -passout pass:sucka -des3 -out #{host_name}-ca.key 2048"
-  System.shell(openssl_genrsa, cd: cert_dir, into: IO.stream())
-  # Generate root certificate
-  "openssl req -passin pass:sucka -x509 -new -nodes -key #{host_name}-ca.key -sha256 -days 825 -out #{host_name}-ca.pem"
-  |> System.shell(cd: cert_dir, into: IO.stream())
+
+  # Generate a Private Key
+  "openssl genrsa -passout pass:sucka -des3 -out #{app_name}_ca.key 2048"
+  |> System.shell(shell_args)
+  # Generate Config File
+  ca_config = EEx.eval_file ca_config_template, app_name: app_name,
+                                                host_name: host_name
+  File.write!("releases/self_signed_cert/ca.conf", ca_config)
+  # Generate Root Certificate
+  "openssl req -config ca.conf -passin pass:sucka -x509 -new -nodes -key #{app_name}_ca.key -sha256 -days 825 -out #{app_name}_ca.pem"
+  |> System.shell(shell_args)
+
+  ######################
+  # Create CA-signed certs
+  ######################
+
+  # Generate a Private Key
+  "openssl genrsa -out #{app_name}.key 2048"
+  |> System.shell(shell_args)
+  # Create a certificate-signing request
+  signing_request_config = EEx.eval_file signing_request_config_template, app_name: app_name,
+                                                                          host_name: host_name
+  File.write!("releases/self_signed_cert/signing_request.conf", signing_request_config)
+  "openssl req -new -config signing_request.conf -key #{app_name}.key -out #{app_name}.csr"
+  |> System.shell(shell_args)
+  # Create an Extensions Config
+  extensions_config = EEx.eval_file extensions_config_template, app_name: app_name,
+                                                                host_name: host_name
+  File.write!("releases/self_signed_cert/extensions.conf", extensions_config)
+  # Create the Signed Certificate
+  "openssl x509 -passin pass:sucka -req -in #{app_name}.csr -CA #{app_name}_ca.pem -CAkey #{app_name}_ca.key -CAcreateserial -out #{app_name}.crt -days 825 -sha256 -extfile extensions.conf"
+  |> System.shell(shell_args)
+
+  ######################
+  # Verify the Cert
+  ######################
+
+  UI.info(IO.ANSI.cyan())
+  "openssl verify -CAfile #{app_name}_ca.pem -verify_hostname #{host_name} #{app_name}.crt"
+  |> System.shell(shell_args)
+  UI.info(IO.ANSI.reset())
 end

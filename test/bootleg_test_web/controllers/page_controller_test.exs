596991dc706104f7fa949ff3720f81ea46311708
Andrew Dixon <dixon@largearcade.com>
2026-05-21 16:26:07 -0700

    Update Copy

--- test/bootleg_test_web/controllers/page_controller_test.exs
+++ test/bootleg_test_web/controllers/page_controller_test.exs
@@ -4,6 +4,6 @@ defmodule BootlegTestWeb.PageControllerTest do
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
     assert html_response(conn, 200) =~ "Deploy everything from development to production."
-    assert html_response(conn, 200) =~ "Testing Claude Code"
+    assert html_response(conn, 200) =~ "Updated with Claude Code"
   end
 end
